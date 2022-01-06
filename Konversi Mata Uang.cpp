64bfdce0705ea76724bb6631c4acccf25d70cb26
FerliMalkanAmien <92583458+FerliMalkanAmien@users.noreply.github.com>
2022-01-06 15:43:16 +0700

    Add files via upload

---
+++ Konversi Mata Uang.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <conio.h>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+    int Pilihan, Rupiah, US_Dollar, Euro, Poundsterling;
+
+
+    do {
+        cout << "==================================" << endl;
+        cout << "   Aplikasi Konversi Mata Uang    " << endl;
+        cout << "==================================" << endl;
+
+        cout << endl;
+
+        cout << "Menu : " << endl;
+        cout << "1. Konversi Mata Uang" << endl;
+        cout << "2. Konverwsi Cryptocurrency" << endl;
+        cout << "Piilihan : " << endl;
+        cin  >>  Pilihan;
+
+        if (Pilihan == 1){
+            cout << "Konversi Mata Uang"
+        }
+
+
+
+    } while (Ulang == 'Y' || Ulang == 'y');
+
+}
